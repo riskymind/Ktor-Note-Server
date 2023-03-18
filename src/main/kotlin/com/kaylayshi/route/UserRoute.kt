e218507001bcf17ad42a565ddc28800137c50f4e
kelechi <oparaiskelechi@gmail.com>
2023-03-19 00:09:02 +0100

    create login endpoing

--- src/main/kotlin/com/kaylayshi/route/UserRoute.kt
+++ src/main/kotlin/com/kaylayshi/route/UserRoute.kt
@@ -2,6 +2,7 @@ package com.kaylayshi.route
 
 import com.kaylayshi.authentication.JwtService
 import com.kaylayshi.data.model.User
+import com.kaylayshi.data.requests.LoginRequest
 import com.kaylayshi.data.requests.RegisterRequest
 import com.kaylayshi.data.response.SimpleResponse
 import com.kaylayshi.repository.AuthRepo
@@ -36,6 +37,31 @@ fun Route.userRoute(
             call.respond(HttpStatusCode.OK, SimpleResponse(true, jwtService.generateToken(user)))
         } catch (e: Exception) {
             call.respond(HttpStatusCode.Conflict, SimpleResponse(true, e.message ?: "An error occurred"))
+        }
+    }
+
+    // Login [User] Endpoint
+    post("/v1/users/login") {
+        val loginRequest = try {
+            call.receive<LoginRequest>()
+        } catch (e: Exception) {
+            call.respond(HttpStatusCode.BadRequest, SimpleResponse(false, "Missing fields"))
+            return@post
+        }
+
+        try {
+            val user = db.findUserByEmail(loginRequest.email)
+            if (user == null) {
+                call.respond(HttpStatusCode.BadRequest, SimpleResponse(false, "Wrong Email id"))
+            } else {
+                if (user.password == hashFunction(loginRequest.password)) {
+                    call.respond(HttpStatusCode.OK, SimpleResponse(true, jwtService.generateToken(user)))
+                } else {
+                    call.respond(HttpStatusCode.BadRequest, SimpleResponse(false, "Incorrect password!!"))
+                }
+            }
+        } catch (e: Exception) {
+            call.respond(HttpStatusCode.Conflict, SimpleResponse(false, e.message ?: "An Error occurred"))
         }
     }
 }
